2ba6f149adf3a079ae094d08a1a72c583de8c4fc
sanuv9683 <sanuv9683@gmail.com>
2023-01-06 11:14:13 +0530

    Content Updated with Spring Beans

--- SpringFramework/01_Hello_Spring/src/lk/ijse/spring/pojo/PojoOne.java
+++ SpringFramework/01_Hello_Spring/src/lk/ijse/spring/pojo/PojoOne.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class PojoOne {
     public PojoOne(){
-        System.out.println("PoJoOne: Instantiated");
+        System.out.println("PojoOne: Instantiated");
     }
 }
