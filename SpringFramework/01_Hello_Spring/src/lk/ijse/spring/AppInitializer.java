6facb17e1150544ea163921d9b354951fe07d297
sanuv9683 <sanuv9683@gmail.com>
2023-01-06 11:35:17 +0530

    Context Invocation

--- SpringFramework/01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ SpringFramework/01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.Customer;
+import lk.ijse.spring.pojo.DBConnection;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -8,6 +10,22 @@ public class AppInitializer {
         AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
+
+        //Context Invocation
+        Customer c1 = ctx.getBean(Customer.class);
+        Customer c2 = ctx.getBean(Customer.class);
+        System.out.println(c1);
+        System.out.println(c2);
+        c1.getCustomerName();
+
+        DBConnection db1 = ctx.getBean(DBConnection.class);
+        DBConnection db2 = ctx.getBean(DBConnection.class);
+        System.out.println(db1);
+        System.out.println(db2);
+
+
+
+
         ctx.close();
     }
 }
