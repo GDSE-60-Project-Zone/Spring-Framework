396c2905b313c048b936c25734e7b526cc8e7383
sanuv9683 <sanuv9683@gmail.com>
2023-01-13 15:13:21 +0530

    introduced @Value annotation

--- SpringFramework/07_Runtime_Value_Injection/src/lk/ijse/spring/config/AppConfig.java
+++ SpringFramework/07_Runtime_Value_Injection/src/lk/ijse/spring/config/AppConfig.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
 public class AppConfig {
+
 
 }
