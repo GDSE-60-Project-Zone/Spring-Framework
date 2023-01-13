b9c7c42a63464a8d5b3b54e97563f7b2bf9d261a
sanuv9683 <sanuv9683@gmail.com>
2023-01-13 15:26:51 +0530

    required false with @Autowired annotation

--- SpringFramework/07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
+++ SpringFramework/07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,17 @@ public class Customer {
 //        System.out.println("Customer:Instantiated "+name);
 //    }
 
-    public Customer(@Value("Dasun,Kasun,Amal") String names[]){
+    @Autowired(required = false)//here spring runs the greedious constructor
+    public Customer(@Value("Dasun,Kasun,Amal") String names[],@Value("1") int i,@Value("2") int c){
         System.out.println("Customer:Instantiated");
         for (String name : names) {
             System.out.println(name);
         }
+    }
+
+    @Autowired(required = false)
+    public Customer(@Value("Dasun") String name,@Value("10") int i){
+        System.out.println("Customer:Instantiated :"+name);
     }
 
     //what you can assign with @Value annotation
