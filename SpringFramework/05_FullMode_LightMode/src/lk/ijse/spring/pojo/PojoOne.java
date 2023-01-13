4d8163bc7355385c17e8224376c18184e12216a2
sanuv9683 <sanuv9683@gmail.com>
2023-01-13 14:01:26 +0530

    Light Mode Introduced

--- SpringFramework/05_FullMode_LightMode/src/lk/ijse/spring/pojo/PojoOne.java
+++ SpringFramework/05_FullMode_LightMode/src/lk/ijse/spring/pojo/PojoOne.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,5 +38,22 @@ public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationCont
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("PojoOne:Application Context Aware");
+    }
+
+
+    //Light Mode
+    @Bean
+    public PojoTwo pojoTwo(){
+        //inter-bean dependencies invocation
+        PojoThree pojoThree1 = pojoThree();
+        PojoThree pojoThree2 = pojoThree();
+        System.out.println(pojoThree1);
+        System.out.println(pojoThree2);
+        return new PojoTwo();
+    }
+
+    @Bean
+    public PojoThree pojoThree(){
+        return new PojoThree();
     }
 }
