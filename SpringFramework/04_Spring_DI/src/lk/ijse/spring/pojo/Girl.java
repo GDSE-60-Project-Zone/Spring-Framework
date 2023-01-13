a8c4ae5981cf61bc4ea0679e7f976d5ebe7b215c
sanuv9683 <sanuv9683@gmail.com>
2023-01-13 11:24:54 +0530

    unnecessory method removed

--- SpringFramework/04_Spring_DI/src/lk/ijse/spring/pojo/Girl.java
+++ SpringFramework/04_Spring_DI/src/lk/ijse/spring/pojo/Girl.java
@@ -1,44 +1,17 @@
 package lk.ijse.spring.pojo;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Girl implements GoodGirl,BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
-    public Girl(){
+public class Girl implements GoodGirl {
+    public Girl() {
         System.out.println("Girl:Instantiated");
     }
 
     @Override
-    public void chat(){
+    public void chat() {
         System.out.println("Hello hello haai");
     }
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("Girl: Bean Factory Aware");
-    }
 
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("Girl: Bean Name Aware");
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("Girl: Disposable Bean");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Girl: Initializing Bean");
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Girl: Application Context Aware Aware");
-    }
 }
