dd6de558c421d9b290538ff6dea1e445f9547253
sanuv9683 <sanuv9683@gmail.com>
2023-01-20 16:59:01 +0530

    Content Updated with Media Type

--- SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/HController.java
+++ SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/HController.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,8 @@ public class HController {
 
     //We can narrow down request using headers also
 
-    @GetMapping(consumes = {"application/json"})
+//    @GetMapping(consumes = {"application/json"})
+    @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public String testOne(){
         return "Method One Invoked";
     }
