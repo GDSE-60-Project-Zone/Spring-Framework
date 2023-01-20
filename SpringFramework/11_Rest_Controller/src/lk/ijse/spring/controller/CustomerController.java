31aaa70560e36ec0349d7b46bbf3b646670cfc39
sanuv9683 <sanuv9683@gmail.com>
2023-01-20 14:17:39 +0530

    Ambiguos Mapping Occured

--- SpringFramework/11_Rest_Controller/src/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/11_Rest_Controller/src/lk/ijse/spring/controller/CustomerController.java
@@ -1,4 +1,22 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @GetMapping
+    public String testOne(){
+        // we can return any compatible data type with rest controllers.
+        //no need of view resolvers
+        return "Hello";
+    }
+
+    @GetMapping
+    public String testTwo(){
+        return "Hello";
+    }
 }
