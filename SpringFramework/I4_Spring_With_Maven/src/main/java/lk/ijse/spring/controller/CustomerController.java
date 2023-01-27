3d97b52cfb2861b89678a7c51aa90e5fb08fa67c
sanuv9683 <sanuv9683@gmail.com>
2023-01-27 16:11:06 +0530

    Refactored CustomerController with Front End

--- SpringFramework/I4_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/I4_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,22 +1,46 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+//response format
+//state
+//message
+//data
 
 @RestController
 @CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
 
-
-    //response format
-    //state
-    //message
-    //data
-
     @PostMapping
-    public void saveCustomer(CustomerDTO dto){
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO dto){
         System.out.println(dto.toString());
+        return new ResponseUtil("OK","Successfully Registered.!",null);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public ResponseUtil deleteCustomer(@RequestParam String id){
+        return new ResponseUtil("OK","Successfully Deleted. :"+id ,null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto){
+        return new ResponseUtil("OK","Successfully Updated. :"+dto.getId() ,null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllCustomers(){
+        ArrayList<CustomerDTO> arrayList= new ArrayList<>();
+        arrayList.add(new CustomerDTO("C001","Ushan","Galle",1000));
+        arrayList.add(new CustomerDTO("C002","Ashan","Galle",2000));
+        arrayList.add(new CustomerDTO("C003","Malshan","Panadura",3000));
+        arrayList.add(new CustomerDTO("C004","Kalshan","Kaluthara",4000));
+        arrayList.add(new CustomerDTO("C005","Rashan","Panaudra",5000));
+        return new ResponseUtil("OK","Successfully Loaded. :" ,arrayList);
     }
 
 }
