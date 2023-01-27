ae96594f49ceeacaa2c142a3c3723e8969ae8385
sanuv9683 <sanuv9683@gmail.com>
2023-01-27 16:24:48 +0530

    Content Updated

--- SpringFramework/I4_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/ItemController.java
+++ SpringFramework/I4_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -3,6 +3,7 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.ItemDTO;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,23 +13,25 @@ import java.util.ArrayList;
 @RequestMapping("/item")
 public class ItemController {
 
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseUtil saveCustomer(@ModelAttribute ItemDTO dto){
+    public ResponseUtil saveItem(@ModelAttribute ItemDTO dto){
         return new ResponseUtil("OK","Successfully Registered.!",null);
     }
 
     @DeleteMapping(params = {"code"})
-    public ResponseUtil deleteCustomer(@RequestParam String code){
-        return new ResponseUtil("OK","Successfully Deleted. :"+code ,null);
+    public ResponseUtil deleteItem(@RequestParam String code){
+        return new ResponseUtil("OK","Successfully Deleted. :"+code  ,null);
     }
 
     @PutMapping
-    public ResponseUtil updateCustomer(@RequestBody ItemDTO dto){
+    public ResponseUtil updateItem(@RequestBody ItemDTO dto){
         return new ResponseUtil("OK","Successfully Updated. :"+dto.getCode() ,null);
     }
 
     @GetMapping
-    public ResponseUtil getAllCustomers(){
+    public ResponseUtil getAllItems(){
         ArrayList<ItemDTO> arrayList= new ArrayList<>();
         arrayList.add(new ItemDTO("I001","Lux",100,1000));
         arrayList.add(new ItemDTO("I002","Signal",200,2000));
