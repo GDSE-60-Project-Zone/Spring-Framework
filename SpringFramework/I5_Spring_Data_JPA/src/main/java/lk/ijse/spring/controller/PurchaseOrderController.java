ec809ad32a5cf0ddf7d32ca59ace1c79288cb890
sanuv9683 <sanuv9683@gmail.com>
2023-02-03 15:06:23 +0530

    Content Updated

--- SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/PurchaseOrderController.java
+++ SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/PurchaseOrderController.java
@@ -1,7 +1,14 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.OrdersDTO;
+import lk.ijse.spring.entity.Item;
+import lk.ijse.spring.entity.OrderDetails;
+import lk.ijse.spring.entity.Orders;
+import lk.ijse.spring.repo.ItemRepo;
+import lk.ijse.spring.repo.OrdersRepo;
 import lk.ijse.spring.util.ResponseUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,10 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class PurchaseOrderController {
 
+    @Autowired
+    private OrdersRepo repo;
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @PostMapping
     public ResponseUtil purchaseOrder(@RequestBody OrdersDTO dto){
-        System.out.println(dto.toString());
-        return new ResponseUtil();
+        Orders ord = mapper.map(dto, Orders.class);
+        if (repo.existsById(ord.getOid())) {
+            throw new RuntimeException("Order"+ord.getOid()+" Already added.!");
+        }
+        repo.save(ord);
+
+        //Update Item Qty
+        for (OrderDetails od : ord.getOrderDetails()) {
+            Item item = itemRepo.findById(od.getItemCode()).get();
+            item.setQtyOnHand(item.getQtyOnHand()-od.getQty());
+            itemRepo.save(item);
+        }
+        return new ResponseUtil("Ok","Successfully Purchased.!",null);
     }
 
 }
