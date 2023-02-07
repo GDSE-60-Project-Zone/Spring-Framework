25e5d6bb972413e5235afd1b6ba5b67b9a75a012
sanuv9683 <sanuv9683@gmail.com>
2023-02-07 10:44:34 +0530

    Introduce about query methods return types

--- SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -3,9 +3,23 @@ package lk.ijse.spring.repo;
 import lk.ijse.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<Customer,String> {
 
     Customer findCustomerByName(String name);
-    Customer findByName(String name);
+    List<Customer> findByName(String name);
+    Customer readCustomerByName(String name);
+    Customer getCustomerByName(String name);
+    Customer queryCustomerByName(String name);
+    Customer searchCustomerByName(String name);
+    Customer streamCustomerByName(String name);
+
+
+
+    Customer countCustomerByName(String name);
+    Customer existsCustomerByName(String name);
+    Customer deleteCustomerByName(String name);
+    Customer removeCustomerByName(String name);
 
 }
--- SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -22,7 +24,33 @@ class CustomerRepoTest {
 
     @Test
     void findByName() {
-        Customer ashan = repo.findCustomerByName("Ashan Fernando");
+        List<Customer> list = repo.findByName("Ashan Fernando");
+        for (Customer customer : list) {
+            System.out.println(customer.toString());
+        }
+    }
+
+    @Test
+    void findCustomerByName() {
+        Customer ashan = repo.findCustomerByName("Kushan Hansaka");
         System.out.println(ashan.toString());
+    }
+
+    @Test
+    void testAllMethods() {
+        Customer c1 = repo.findCustomerByName("Kushan Hansaka");
+        Customer c2 = repo.readCustomerByName("Kushan Hansaka");
+        Customer c3 = repo.getCustomerByName("Kushan Hansaka");
+        Customer c4 = repo.queryCustomerByName("Kushan Hansaka");
+        Customer c5 = repo.searchCustomerByName("Kushan Hansaka");
+        Customer c6 = repo.streamCustomerByName("Kushan Hansaka");
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3.toString());
+        System.out.println(c4.toString());
+        System.out.println(c5.toString());
+        System.out.println(c6.toString());
+
     }
 }
