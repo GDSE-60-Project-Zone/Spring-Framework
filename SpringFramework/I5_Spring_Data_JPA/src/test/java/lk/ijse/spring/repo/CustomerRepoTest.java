c90eb318db3ce68ea17068338628de9a114a3110
sanuv9683 <sanuv9683@gmail.com>
2023-02-07 10:16:10 +0530

    First testing method invoked

--- SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -1,7 +1,10 @@
 package lk.ijse.spring.repo;
 
+import lk.ijse.spring.config.WebRootConfig;
+import lk.ijse.spring.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -10,12 +13,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @WebAppConfiguration//creat the testing context
-@ContextConfiguration // add configuration for that context
+@ContextConfiguration(classes = {WebRootConfig.class}) // add configuration for that context
 @ExtendWith(SpringExtension.class) // integrate junit with Spring
 class CustomerRepoTest {
 
+    @Autowired
+    CustomerRepo repo;
+
     @Test
     void findByName() {
-
+        Customer ashan = repo.findCustomerByName("Ashan Fernando");
+        System.out.println(ashan.toString());
     }
 }
